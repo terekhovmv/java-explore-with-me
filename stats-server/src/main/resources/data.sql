ddf9ad91d247559dfafd646717b086c20523af96
Max.Terekhov <terekhovmv@yandex.ru>
2023-02-12 10:52:26 +0300

    infr: initial debug data

--- stats-server/src/main/resources/data.sql
+++ stats-server/src/main/resources/data.sql
@@ -13,3 +13,19 @@ ALTER TABLE uris
 
 ALTER TABLE hits
     ALTER COLUMN id RESTART WITH 1;
+
+INSERT INTO apps(name)
+VALUES ('app1'),
+       ('app2');
+INSERT INTO uris(path)
+VALUES ('uri1'),
+       ('uri2'),
+       ('uri3');
+INSERT INTO hits(app_id, uri_id, ip)
+VALUES (1, 1, '127.0.0.1'),
+       (1, 1, '127.0.0.1'),
+       (1, 2, '127.0.0.1'),
+       (1, 3, '127.0.0.1'),
+       (1, 1, '192.168.1.1'),
+       (2, 3, '127.0.0.1'),
+       (2, 3, '192.168.1.1');
