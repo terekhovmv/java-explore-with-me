80c30259c2fbcceb157714ade6d9024bba5d3919
Max.Terekhov <terekhovmv@yandex.ru>
2023-02-14 17:58:38 +0300

    feat: summary endpoint

--- stats-server/src/main/java/ru/practicum/ewm/stats/StatsController.java
+++ stats-server/src/main/java/ru/practicum/ewm/stats/StatsController.java
@@ -1,23 +1,38 @@
 package ru.practicum.ewm.stats;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.stats.dto.HitDto;
+import ru.practicum.ewm.stats.dto.SummaryDto;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class StatsController {
     private final StatsService service;
 
-    @PostMapping(value = "/hit")
+    @PostMapping("/hit")
     @ResponseStatus(HttpStatus.CREATED)
     HitDto register(@RequestBody @Valid HitDto dto) {
         return service.register(dto);
     }
+
+    @GetMapping("/stats")
+    @ResponseStatus(HttpStatus.OK)
+    List<SummaryDto> getStats(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+            @RequestParam List<String> uris,
+            @RequestParam(required = false, defaultValue = "false") boolean unique
+    ) {
+        return service.getStats(start, end, uris, unique);
+    }
+
 }
