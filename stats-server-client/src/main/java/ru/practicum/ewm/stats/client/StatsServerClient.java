f3ea3d8bcbc3c1633cd0b4fd56c21ce38b6ea986
Max.Terekhov <terekhovmv@yandex.ru>
2023-02-15 18:53:38 +0300

    feat: adds initial version of stats server client lib

---
+++ stats-server-client/src/main/java/ru/practicum/ewm/stats/client/StatsServerClient.java
@@ -0,0 +1,31 @@
+package ru.practicum.ewm.stats.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+@Slf4j
+@Service
+public class StatsServerClient {
+
+    protected final RestTemplate rest;
+
+    public StatsServerClient(
+            @Value("${stats-server.url}") String serverUrl,
+            @Value("${stats-server.app-name}") String appName,
+            RestTemplateBuilder builder
+    ) {
+        rest = builder
+                .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                .build();
+    }
+
+    public void hit(String uri, String ip) {
+        log.debug("Call {} from {}", uri, ip);
+    }
+}
