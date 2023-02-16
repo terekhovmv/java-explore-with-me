6cd6cbcc659d82b9adaa8a96ebfa9a0ac4feae90
Max.Terekhov <terekhovmv@yandex.ru>
2023-02-16 18:02:22 +0300

    test: adds service mock tests

--- stats-dto/src/main/java/ru/practicum/stats/dto/SummaryDto.java
+++ stats-dto/src/main/java/ru/practicum/stats/dto/SummaryDto.java
@@ -1,8 +1,13 @@
 package ru.practicum.stats.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SummaryDto {
     String app;
     String uri;
