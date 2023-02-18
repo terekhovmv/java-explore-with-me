5be2609aa780481e3bd0002638231f9a165bc154
Max.Terekhov <terekhovmv@yandex.ru>
2023-02-18 18:17:00 +0300

    infr: initial SQLized data model

---
+++ ewm-server/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+DELETE
+FROM users;
+DELETE
+FROM categories;
+
+ALTER TABLE users
+    ALTER COLUMN id RESTART WITH 1;
+
+ALTER TABLE categories
+    ALTER COLUMN id RESTART WITH 1;
+
+INSERT INTO users(name, email)
+VALUES ('ann', 'ann@abc.def'),
+       ('bob', 'bob@abc.def'),
+       ('cam', 'cam@abc.def');
+
+INSERT INTO categories(name)
+VALUES ('anschlag'),
+       ('business'),
+       ('concerts');
